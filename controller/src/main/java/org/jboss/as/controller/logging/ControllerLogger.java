0570a6d459ebe6bf7561752f00c38714ec84b7c0
Tomaz Cerar <tomaz.cerar@gmail.com>
2015-09-08 15:39:44 +0200

    WFCORE-807 Improve error reporting for missing capabilties

--- controller/src/main/java/org/jboss/as/controller/logging/ControllerLogger.java
+++ controller/src/main/java/org/jboss/as/controller/logging/ControllerLogger.java
@@ -3242,6 +3242,12 @@ public interface ControllerLogger extends BasicLogger {
     @Message(id = NONE, value = "    %s in context '%s'")
     String formattedCapabilityId(String capability, String context);
 
+    @Message(id = NONE, value = ", possible registration points are: %s")
+    String possibleCapabilityProviderPoints(String providerPoints);
+
+    @Message(id = NONE, value = ", there are no known registration points that could provide it.")
+    String noKnownProviderPoints();
+
     @Message(id = 370, value="Incomplete expression: %s")
     OperationFailedException incompleteExpression(String expression);
 
